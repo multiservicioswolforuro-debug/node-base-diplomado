77fef87351547e521cde23c481e345eb26e76782
Juan Jose Callahuara <multiservicioswolforuro@gmail.com>
2026-03-16 18:55:55 -0400

    add swagger endpoint

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,6 +3,16 @@ import { listPagination } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/users/list/pagination:
+ *   get:
+ *     summary: Obtener usuarios con paginación
+ *     description: Retorna la lista de usuarios con paginación
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ */
 router.get("/list/pagination", listPagination);
 
 export default router;
